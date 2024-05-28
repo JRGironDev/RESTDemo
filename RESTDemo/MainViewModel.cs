a6defd8abb02fd9bfcaa475216f242d13aae36d4
José Quiñónez <joserobertoq@outlook.com>
2024-05-28 09:32:07 -0600

    Add MainViewModel

---
+++ RESTDemo/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Windows.Input;
+
+namespace RESTDemo;
+
+public class MainViewModel
+{
+    HttpClient client;
+    JsonSerializerOptions _serializerOptions;
+
+    public MainViewModel()
+    {
+        client = new HttpClient();
+        _serializerOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+    }
+
+    public ICommand AddUserCommand => 
+        new Command(() => 
+            {
+
+            });
+}
