6f7c28489728a9449cf05ad77b0378ee63520a5f
José Quiñónez <joserobertoq@outlook.com>
2024-05-28 10:01:16 -0600

    HTTPClient Created

--- RESTDemo/MainViewModel.cs
+++ RESTDemo/MainViewModel.cs
@@ -8,6 +8,8 @@ public class MainViewModel
     HttpClient client;
     JsonSerializerOptions _serializerOptions;
 
+    string baseUrl = "https://6655ef543c1d3b60293ba6fb.mockapi.io";
+
     public MainViewModel()
     {
         client = new HttpClient();
@@ -18,8 +20,19 @@ public class MainViewModel
     }
 
     public ICommand AddUserCommand => 
-        new Command(() => 
+        new Command(async () => 
             {
+                string? url = $"{baseUrl}/users";
+                HttpResponseMessage? response = await client.GetAsync(url);
 
+                if(response.IsSuccessStatusCode)
+                {
+                    //string content = await response.Content.ReadAsStringAsync();
+                    using(var responseStream = 
+                        await response.Content.ReadAsStreamAsync())
+                    {
+                        var data = await JsonSerializer.DeserializeAsync<List<User>>(responseStream, _serializerOptions);
+                    }
+                }
             });
 }
--- RESTDemo/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/RESTDemo.AssemblyInfo.cs
+++ RESTDemo/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/RESTDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RESTDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7c4813a0d7a6ee7b526c087469622ad543eade28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a6defd8abb02fd9bfcaa475216f242d13aae36d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RESTDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RESTDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
